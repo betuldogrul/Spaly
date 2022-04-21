5ee5f0c115efdb7089d52b0c882be74b95e58536
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-04-21 11:31:45 +0300

    edited category and creditcard

--- Category.java
+++ Category.java
@@ -1,8 +1,15 @@
 package Spaly;
 
-public class Category extends CreditCard
+public class Category 
 {
     private String name; 
     private double currentSpendings;
+    private CreditCard card;
 
+    private Category(String name, double currentSpendings, CreditCard card)
+    {
+        this.name = name;
+        this.currentSpendings = currentSpendings;
+        this.card = card;
+    }
 }
--- CreditCard.java
+++ CreditCard.java
@@ -2,23 +2,28 @@ package Spaly;
 
 public class CreditCard 
 {
-    private int number;
-    private long limit;
-    private long totalMoney;
+    private double number;
+    private double upperLimit;
+    private double totalMoney;
 
+    public CreditCard(double number, double upperLimit, double totalMoney)
+    {
+        this.number = number;
+        this.upperLimit = upperLimit;
+        this.totalMoney = totalMoney;
+    }
 
-    public long addMoney(int amount)
+    public double addMoneyToCard(int amount)
     {
         //TODO
         return 0;
     }
 
-    public long removeMoney(int amount)
+    public double removeMoneyFromCard(int amount)
     {
         //TODO
         return 0;
     }
-
 
     //A method to interact with the database
 }
