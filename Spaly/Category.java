c436b3bb2debec4d35c363548ae8f837b6b67abd
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-04-21 13:17:09 +0300

    added setter and getter methods

--- Spaly/Category.java
+++ Spaly/Category.java
@@ -3,13 +3,33 @@ package Spaly;
 public class Category 
 {
     private String name; 
-    private double currentSpendings;
+    private double currentSpending;
     private CreditCard card;
 
     private Category(String name, double currentSpendings, CreditCard card)
     {
         this.name = name;
-        this.currentSpendings = currentSpendings;
+        this.currentSpending = currentSpendings;
         this.card = card;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public double getCurrentSpending()
+    {
+        return currentSpending;
+    }
+
+    public CreditCard getCreditCard()
+    {
+        return this.card;
+    }
+
+    public void addSpending(int amount)
+    {
+        currentSpending = currentSpending + amount;
     }
 }
--- Spaly/CreditCard.java
+++ Spaly/CreditCard.java
@@ -2,27 +2,42 @@ package Spaly;
 
 public class CreditCard 
 {
-    private double number;
+    private double cardNumber;
     private double upperLimit;
-    private double totalMoney;
+    private double totalSpentMoney;
 
     public CreditCard(double number, double upperLimit, double totalMoney)
     {
-        this.number = number;
+        this.cardNumber = number;
         this.upperLimit = upperLimit;
-        this.totalMoney = totalMoney;
+        this.totalSpentMoney = totalMoney;
+    }
+
+    public double getCardNumber()
+    {
+        return cardNumber;
+    }
+
+    public double getUpperLimit()
+    {
+        return this.upperLimit;
+    }
+
+    public double getTotalSpentMoney()
+    {
+        return this.totalSpentMoney;
     }
 
     public double addMoneyToCard(int amount)
     {
-        totalMoney = totalMoney + amount;
-        return totalMoney;
+        totalSpentMoney = totalSpentMoney + amount;
+        return totalSpentMoney;
     }
 
     public double removeMoneyFromCard(int amount)
     {
-        totalMoney = totalMoney - amount;
-        return totalMoney;
+        totalSpentMoney = totalSpentMoney - amount;
+        return totalSpentMoney;
     }
 
     //A method to interact with the database
