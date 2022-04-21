c37d60d5ae02afb573940f3161b2ddd9af0e221c
betuldogrul <91432039+betuldogrul@users.noreply.github.com>
2022-04-21 09:51:54 +0300

    initial commit

---
+++ CreditCard.java
@@ -0,0 +1,21 @@
+public class CreditCard 
+{
+    int number;
+    long limit;
+    long totalMoney;
+
+
+    public long addMoney(int amount)
+    {
+        //TODO
+        return 0;
+    }
+
+    public long removeMoney(int amount)
+    {
+        //TODO
+        return 0;
+    }
+
+    //A method to interact with the database
+}
