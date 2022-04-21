30226e615a36aad2252a1b8c8b6ec539bbf0f048
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-04-21 13:04:30 +0300

    add method added to categories

--- Spaly/Categories.java
+++ Spaly/Categories.java
@@ -5,4 +5,14 @@ import java.util.ArrayList;
 public class Categories 
 {
     private ArrayList<Category> categories;
+
+    private Categories()
+    {
+        categories  = new ArrayList<Category>();
+    }
+
+    public void addToCategories(Category cat)
+    {
+        categories.add(cat);
+    }
 }
