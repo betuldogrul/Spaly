31a30779eaa265b55d57d6be248f4330803d5785
melisa <irem.akel@ug.bilkent.edu.tr>
2022-04-21 11:15:15 +0300

    .

--- ShoppingList.java
+++ ShoppingList.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class ShoppingList {
     private ArrayList<Item> items;
     Goals goal;
+    public ShoppingList()
+    {
+        
+    }
     public void addToGoal(Item item)
     {
         if(goal.getItemsArrayList().size() != 3)
