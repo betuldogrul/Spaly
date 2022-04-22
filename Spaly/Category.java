b8b366a53ae3f76bcb8e7b5521dbb56f112ec3ff
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-04-22 17:14:53 +0300

    Added the percentage interface and its method to some classes which implement it

--- Spaly/Category.java
+++ Spaly/Category.java
@@ -1,13 +1,15 @@
 package Spaly;
 
-public class Category 
+public class Category extends Categories
 {
-    private String name; 
-    private double currentSpending;
+    private String name; // the name of the category 
+    private double currentSpending; // the total spending so far 
     private CreditCard card;
+    private double percentageOfTheCategory;
 
-    private Category(String name, double currentSpendings, CreditCard card)
+    public Category(String name, double currentSpendings, CreditCard card)
     {
+        super();
         this.name = name;
         this.currentSpending = currentSpendings;
         this.card = card;
@@ -31,5 +33,19 @@ public class Category
     public void addSpending(int amount)
     {
         currentSpending = currentSpending + amount;
+    }
+
+    public void pieceCalculator() // calculates and stores the percentages of each category
+    {
+        int sum = 0;
+        for(int i = 0; i < super.categories.size(); i++)
+        {
+            sum =  sum + super.categories.get[i];
+        }
+
+        for(int j = 0; j < super.categories.size(); j++)
+        {
+            super.percentageOfCategories.add[j] = (super.categories.get[j] * 100) / sum;
+        }
     }
 }
