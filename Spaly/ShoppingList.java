e6f946f84174110469af1dd252ff8a50f3ad2182
melisa <irem.akel@ug.bilkent.edu.tr>
2022-04-24 12:42:12 +0300

    shopping list

--- Spaly/ShoppingList.java
+++ Spaly/ShoppingList.java
@@ -3,16 +3,17 @@ package Spaly;
 import java.util.ArrayList;
 
 public class ShoppingList {
-    private ArrayList<ArrayList<Item>> items;// this arraylist will be created by using sql
-    Goals goal;
+    private ArrayList<ArrayList<Item>> AllItems;// this arraylist will be created by using sql
+    Goals goal;//aggragation
 
     public ShoppingList() {
-        items = new ArrayList<ArrayList<Item>>();// using sql it will be created arraylist of items of arraylist 
+        AllItems = new ArrayList<ArrayList<Item>>();// using sql it will be created arraylist of items of arraylist 
         //inside arraylist will keep for same product in different website
         //outside will keep different products
     }
 
     public void addToGoal(Item item) {
+        //this item will come in goal
         if (goal.getItemsArrayList().size() != 3) {
             targetedItem tItem = new targetedItem(item.getName(), item.getPrice(), item.getImage(), true, item.getWebsite());
             goal.getItemsArrayList().add(tItem);
@@ -22,13 +23,13 @@ public class ShoppingList {
     }
 
     public ArrayList<Item> search(String name) {
-        for(int i = 0; i < items.size(); i++)
+        for(int i = 0; i < AllItems.size(); i++)
         {
             // it will take from SQL products and when user search for an item it will bring the desired arrayList
-            if(name == items.get(i).get(i).getName())//searched after searching from our arraylist with an for loop we will 
+            if(name == AllItems.get(i).get(i).getName())//searched after searching from our arraylist with an for loop we will 
             //return this arraylist and show the user different website for the same products.
             {
-               return items.get(i);
+               return AllItems.get(i);
             }
         }
             System.out.println("Spaly doesn't have that product yet. Please wait later versions.");
