3f739286e3e9f5b7931ed0ac86ce132170637582
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-05-08 14:22:10 +0300

    small changes in the creditcard and profile

--- Spaly/Profile.java
+++ Spaly/Profile.java
@@ -4,15 +4,17 @@ public class Profile
 {
     private String userName;
     private String password;
+    private String email_address;
 
     public Profile()
     {
         
     }
-    public Profile(String userName, String password)
+    public Profile(String userName, String password, String email_address)
     {
         this.userName = userName;
         this.password = password;
+        this.email_address = email_address;
         //these will be added to the database
     }
 
