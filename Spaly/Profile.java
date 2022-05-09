bc369bb30096e021a4ad90164470474459f8d61e
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-05-10 02:08:47 +0300

    I have added sql code profile class and made changes in the cardstatement method

--- Spaly/Profile.java
+++ Spaly/Profile.java
@@ -1,40 +1,83 @@
 package Spaly;
 
+import java.sql.ResultSet;
+
 public class Profile 
 {
-    private String userName;
-    private String password;
-    private String email_address;
+    private ArrayList<User> users;
 
     public Profile()
     {
-        
-    }
-    public Profile(String userName, String password, String email_address)
-    {
-        this.userName = userName;
-        this.password = password;
-        this.email_address = email_address;
-        //these will be added to the database
+       users =  new ArrayList<User>();
+       try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        final String DbUrl = "jdbc:mysql://localhost:3306/melisa";
+        final String username = "root";
+        final String password = "74252002";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, username, password);
+            String sql = "SELECT * FROM users";
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            while (rs.next()) {
+                int user_id = rs.getInt("user_id");
+                int income = rs.getInt("income");
+                String user_name = rs.getString("username");
+                String first_name = rs.getString("first_name");
+                String last_name = rs.getString("last_name");
+                String userpassword = rs.getString("password");
+                String email = rs.getString("email");
+                User user = new User(user_id, income, user_name, first_name, last_name, userpassword, email);
+                users.add(user);
+
+            }
+            p.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        } 
     }
 
-    public boolean login(String userName, String password)
+    public boolean login(String userName, String userpassword)
     {
-        if(this.userName.equals(userName))
-        {
-            if(this.password.equals(password))
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        final String DbUrl = "jdbc:mysql://localhost:3306/melisa";
+        final String username = "root";
+        final String password = "74252002";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, username, password);
+            String sql = "SELECT * FROM users WHERE username = " + userName + " AND password = " + userpassword;
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+            if(rs = "NULL")
             {
-                return true;
+                System.out.println("Invalid password or user_id. Please try again.");
             }
             else
             {
-                System.out.println("Wrong password!");
+                //ana ekrana yönlendir 
             }
-        }
-        else
-        {
-            System.out.println("User not found. Please create an account.");
-        }
-        return false;
+            p.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        } 
+    }
+
+    public void addNewProfile(int id, int income, String userName, String userSurname, String userUserName, String password, String userMail)
+    {
+        User user = new User(id, income, userName, userSurname, userUserName, password, userMail);
     }
 }
--- Spaly/Spaly/src/CardStatement.java
+++ Spaly/Spaly/src/CardStatement.java
@@ -16,6 +16,36 @@ public class CardStatement // a class to hold all credit cards
     public CardStatement()
     {
         cards = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        final String DbUrl = "jdbc:mysql://localhost:3306/melisa";
+        final String username = "root";
+        final String password = "74252002";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, username, password);
+            String sql = "SELECT * FROM cardstatement";
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            while (rs.next()) {
+                int creditcard_id = rs.getInt("creditcard_id");
+                String card_number = rs.getString("card_number");
+                String card_bank = rs.getString("card_bank");
+                int card_limit = rs.getInt("card_limit");
+                int user_id = rs.getInt("user_id");
+                CreditCard card = new CreditCard(creditcard_id, card_number, card_limit, )
+                cards.add();
+            }
+            p.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public void addCreditCard(CreditCard c)
