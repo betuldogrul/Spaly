258872a977bc372fa4adfa8b85e30d8a3ed7a14a
betuldogrul <betul.dogrul@ug.bilkent.edu.tr>
2022-05-10 22:14:32 +0300

    I have made changes about sql

--- Spaly/Spaly/src/CardStatement.java
+++ Spaly/Spaly/src/CardStatement.java
@@ -1,5 +1,3 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,11 +7,12 @@ import java.util.ArrayList;
 /**
  * CardStatement
  */
-public class CardStatement // a class to hold all credit cards 
+public class CardStatement // a class to hold all credit cards which belongs to one user 
 {
+    private double totalSpending;
     private ArrayList<CreditCard> cards;
 
-    public CardStatement()
+    public CardStatement(int userId)
     {
         cards = new ArrayList<>();
         try{
@@ -28,7 +27,7 @@ public class CardStatement // a class to hold all credit cards
         ResultSet rs = null;
         try{
             Connection conn = DriverManager.getConnection(DbUrl, username, password);
-            String sql = "SELECT * FROM cardstatement";
+            String sql = "SELECT * FROM cardstatement WHERE user_id = " + userId;
             p = conn.prepareStatement(sql);
             rs = p.executeQuery();
 
@@ -37,9 +36,10 @@ public class CardStatement // a class to hold all credit cards
                 Double card_number = rs.getDouble("card_number");
                 String card_bank = rs.getString("card_bank");
                 int card_limit = rs.getInt("card_limit");
+                int totalSpentMoney = rs.getInt("totalSpentMoney");
                 int user_id = rs.getInt("user_id");
                 //public CreditCard(int creditcard_id, double number, double upperLimit, double totalMoney, String cardBank, int userID)
-                CreditCard card = new CreditCard(creditcard_id, card_number, card_limit,0, card_bank, user_id);
+                CreditCard card = new CreditCard(creditcard_id, card_number, card_limit, totalSpentMoney, card_bank, user_id);
                 cards.add(card);
             }
             p.close();
@@ -47,12 +47,54 @@ public class CardStatement // a class to hold all credit cards
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, username, password);
+            String sql = "SELECT SUM(spending) FROM cardstatement WHERE creditcard_id IN (SELECT creditcard_id FROM credit_card WHERE user_id = " + userId + ")";
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            while(rs.next())
+            {
+                totalSpending = rs.getInt("SUM(spending");
+            }
+
+            p.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public double getTotalSpending()
+    {
+        return this.totalSpending;
     }
 
     public void addCreditCard(CreditCard c)
     {
         cards.add(c);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        final String DbUrl = "jdbc:mysql://localhost:3306/melisa";
+        final String username = "root";
+        final String password = "74252002";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, username, password);
+            String sql = "INSERT INTO credit_card VALUES (" + c.getCreditCardId() + "," + c.getCardNumber() + "," +
+                             c.getCardBank() + "," + c.getUpperLimit() + "," + c.getTotalSpentMoney() + "," + c.getUserIDOfTheCard();
+            p = conn.prepareStatement(sql);
+            rs = p.executeQuery();
+            p.close();
+            conn.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
-
-    // add a method to hold the total spendings karttan bağmsiz bir şekilde 
 }
